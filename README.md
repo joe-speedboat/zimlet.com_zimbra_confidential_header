2e3298c3bf6b1f025cfe628d204dbc13a99b18ee
Chris Ruettimann <chris@bitbull.ch>
2023-06-11 08:55:34 +0200

    first release after testing

---
+++ README.md
@@ -0,0 +1,57 @@
+# Confidential Header Zimlet
+
+The Confidential Header Zimlet is a Zimbra extension that allows users to add and view the Confidential/Sensitivity Header based on RFC2156.
+
+## Description
+
+This Zimlet adds a customizable infobar to the message view, indicating the sensitivity level of the email. It also provides a sensitivity button in the compose toolbar, allowing users to set the sensitivity level for outgoing emails.
+
+## Installation
+
+1. Download the Zimlet package (`com_zimbra_confidential_header.zip`) from the [releases page](https://github.com/your-username/your-repo/releases).
+2. Log in to your Zimbra server as an administrator.
+3. Navigate to the **Admin Console**.
+4. Go to **Zimlets**.
+5. Click on **Install Zimlet**.
+6. Select the downloaded `com_zimbra_confidential_header.zip` file.
+7. Click on **OK** to install the Zimlet.
+8. Reload the Zimbra server.
+
+## Usage
+
+### Viewing Sensitivity in Message View
+
+When viewing an email, the sensitivity level will be displayed in an infobar at the top of the message view. The infobar will show the corresponding sensitivity icon and text based on the sensitivity level of the email.
+
+### Setting Sensitivity in Compose View
+
+When composing a new email or replying to an email, the compose toolbar will include a sensitivity button. Clicking on the sensitivity button will open a menu with different sensitivity options. Select the desired sensitivity level for the email from the menu.
+
+### Sensitivity Levels
+
+The Zimlet supports the following sensitivity levels:
+
+- Normal
+- Company-Confidential
+- Personal
+- Private
+
+## Development
+
+To build and customize the Confidential Header Zimlet, follow these steps:
+
+1. Clone the repository: `git clone https://github.com/your-username/your-repo.git`
+2. Modify the Zimlet files in the `zimlet/com_zimbra_confidential_header` directory to add or modify functionality.
+3. Update the Zimlet version and other details in the `zimlet/com_zimbra_confidential_header/com_zimbra_confidential_header.xml` file.
+4. Build the Zimlet package: `./build.sh`
+5. The built Zimlet package (`com_zimbra_confidential_header.zip`) will be available in the `build` directory.
+6. Follow the installation instructions above to install the modified Zimlet package on your Zimbra server.
+
+## Contributions
+
+Contributions to the Confidential Header Zimlet are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request.
+
+## License
+
+The Confidential Header Zimlet is released under the [MIT License](https://opensource.org/licenses/MIT).
+
---
+++ zimlet/com_zimbra_confidential_header/com_zimbra_confidential_header.js
@@ -0,0 +1,175 @@
+// Constructor
+function com_zimbra_confidential_header_HandlerObject() {
+   this.defaultSensitivity = ''; // Default sensitivity value
+   this.composeViewId = ''; // ID of the current compose view
+}
+
+com_zimbra_confidential_header_HandlerObject.prototype = new ZmZimletBase();
+com_zimbra_confidential_header_HandlerObject.prototype.constructor = com_zimbra_confidential_header_HandlerObject;
+
+com_zimbra_confidential_header_HandlerObject.prototype.toString = function() {
+   return "com_zimbra_confidential_header_HandlerObject";
+};
+
+var ConfHeaderZimlet = com_zimbra_confidential_header_HandlerObject;
+
+/**
+ * Initialize the Zimlet.
+ */
+ConfHeaderZimlet.prototype.init = function() {
+   AjxPackage.require({name:"MailCore", callback:new AjxCallback(this, this._applyRequestHeaders)});
+   var zimletInstance = appCtxt._zimletMgr.getZimletByName('com_zimbra_confidential_header').handlerObject;
+   if (!zimletInstance.sensitivity) {
+      zimletInstance.sensitivity = [];
+   }
+};
+
+/**
+ * Apply the Sensitivity header to the mail request headers.
+ */
+ConfHeaderZimlet.prototype._applyRequestHeaders = function() {   
+   ZmMailMsg.requestHeaders["Sensitivity"] = "Sensitivity";
+};
+
+/**
+ * Handle the display of the Confidential/Sensitivity Header in the message view.
+ * @param {ZmMailMsg} msg - The message object.
+ * @param {ZmMailMsg} oldMsg - The previous message object.
+ * @param {ZmMailMsgView} msgView - The message view object.
+ */
+ConfHeaderZimlet.prototype.onMsgView = function (msg, oldMsg, msgView) {
+   var zimletInstance = appCtxt._zimletMgr.getZimletByName('com_zimbra_confidential_header').handlerObject;
+   try {
+      var infoBarDiv = document.getElementById(msgView._infoBarId);      
+      if (infoBarDiv) {
+         if (msg.attrs) {
+            if (msg.attrs['Sensitivity']) {
+               var z = document.createElement('div');
+               switch (msg.attrs['Sensitivity']) {
+                  case 'Personal':
+                     z.innerText = '🤐 ' + zimletInstance.getMessage('ConfHeaderZimlet_Personal');
+                     break;
+                  case 'Private':
+                     z.innerText = '🤐 ' + zimletInstance.getMessage('ConfHeaderZimlet_Private');
+                     break;
+                  default:
+                     z.innerText = '🤐 ' + zimletInstance.getMessage('ConfHeaderZimlet_CompanyConfidential');
+               }
+               z.className = 'ConfHeaderZimlet-infobar';
+               infoBarDiv.insertBefore(z, infoBarDiv.firstChild);
+            }
+         }
+      }
+   } catch (err) {
+      console.log(err);
+   }
+};
+
+/**
+ * Initialize the toolbar and add the sensitivity button for compose views.
+ * @param {ZmApp} app - The Zimbra application object.
+ * @param {ZmToolbar} toolbar - The toolbar object.
+ * @param {ZmController} controller - The controller object.
+ * @param {string} viewId - The ID of the current view.
+ */
+ConfHeaderZimlet.prototype.initializeToolbar = function(app, toolbar, controller, viewId) {
+   // Get the zimlet instance
+   var zimletInstance = appCtxt._zimletMgr.getZimletByName('com_zimbra_confidential_header').handlerObject;
+
+   // Disable DETACH_COMPOSE operation
+   toolbar.enable(ZmOperation.DETACH_COMPOSE, false);
+
+   // Check if the current view is a compose view
+   if (viewId.indexOf("COMPOSE") >= 0) {
+      // Reset sensitivity header when a new compose view is opened
+      zimletInstance.composeViewId = viewId;
+      zimletInstance.resetSensitivity();
+
+      // Check if the sensitivity button is already defined
+      if (toolbar.getButton('SENSITIVITY')) {
+         // Button already defined, return
+         return;
+      }
+
+      // Create the sensitivity options menu
+      var menu = new ZmPopupMenu(toolbar);
+      var menuItemNormal = menu.createMenuItem(Dwt.getNextId(), {image: "Blank_16", text: zimletInstance.getMessage('ConfHeaderZimlet_sensitivityOptionNormal')});
+      var menuItemConfidential = menu.createMenuItem(Dwt.getNextId(), {image: "Blank_16", text: zimletInstance.getMessage('ConfHeaderZimlet_sensitivityOptionConfidential')});
+      var menuItemPrivate = menu.createMenuItem(Dwt.getNextId(), {image: "Blank_16", text: zimletInstance.getMessage('ConfHeaderZimlet_sensitivityOptionPrivate')});
+      var menuItemPersonal = menu.createMenuItem(Dwt.getNextId(), {image: "Blank_16", text: zimletInstance.getMessage('ConfHeaderZimlet_sensitivityOptionPersonal')});
+      menuItemNormal.addSelectionListener(new AjxListener(this, this._selectSensitivity, ["", zimletInstance.getMessage('ConfHeaderZimlet_sensitivityOptionNormal'), toolbar]));
+      menuItemConfidential.addSelectionListener(new AjxListener(this, this._selectSensitivity, ["Company-Confidential", zimletInstance.getMessage('ConfHeaderZimlet_sensitivityOptionConfidential'), toolbar]));
+      menuItemPrivate.addSelectionListener(new AjxListener(this, this._selectSensitivity, ["Private", zimletInstance.getMessage('ConfHeaderZimlet_sensitivityOptionPrivate'), toolbar]));
+      menuItemPersonal.addSelectionListener(new AjxListener(this, this._selectSensitivity, ["Personal", zimletInstance.getMessage('ConfHeaderZimlet_sensitivityOptionPersonal'), toolbar]));
+
+      // Create the sensitivity button
+      var buttonArgs = {
+         text: "🤐  " + zimletInstance.getMessage('ConfHeaderZimlet_sensitivityBtn'),
+         index: 1,
+         showImageInToolbar: false,
+         showTextInToolbar: true,
+         menu: menu
+      };
+      var button = toolbar.createOp("SENSITIVITY", buttonArgs);
+
+      // Store button in Zimlet instance for future use
+      zimletInstance.sensitivityButton = button;
+   }
+};
+
+/**
+ * Handle the selection of a sensitivity option.
+ * @param {string} sensitivity - The selected sensitivity value.
+ * @param {string} displayText - The display text of the selected option.
+ * @param {ZmToolbar} toolbar - The toolbar object.
+ */
+ConfHeaderZimlet.prototype._selectSensitivity = function(sensitivity, displayText, toolbar) {
+   var zimletInstance = appCtxt._zimletMgr.getZimletByName('com_zimbra_confidential_header').handlerObject;
+   zimletInstance.sensitivity[zimletInstance.composeViewId] = sensitivity;
+   // Update button text
+   if (zimletInstance.sensitivityButton) {
+      zimletInstance.sensitivityButton.setText("🤐  " + displayText);
+   }
+};
+
+/**
+ * Reset the sensitivity value when a compose view is closed.
+ */
+ConfHeaderZimlet.prototype.resetSensitivity = function() {
+   var zimletInstance = this;
+   var composeViewId = zimletInstance.composeViewId;
+   if (zimletInstance.sensitivity && zimletInstance.sensitivity[composeViewId]) {
+      zimletInstance.sensitivity[composeViewId] = zimletInstance.defaultSensitivity;
+      if (zimletInstance.sensitivityButton) {
+         zimletInstance.sensitivityButton.setText("🤐  " + zimletInstance.getMessage('ConfHeaderZimlet_sensitivityOptionNormal'));
+      }
+   }
+};
+
+/**
+ * Add the Sensitivity header to the custom mime headers.
+ * @param {array} customHeaders - The custom mime headers array.
+ */
+ConfHeaderZimlet.prototype.addCustomMimeHeaders = function(customHeaders) {
+   var zimletInstance = appCtxt._zimletMgr.getZimletByName('com_zimbra_confidential_header').handlerObject;
+   var composeViewId = zimletInstance.composeViewId;
+   if (zimletInstance.sensitivity && zimletInstance.sensitivity[composeViewId]) {
+      var controller = appCtxt.getCurrentController();
+      var sensitivity = zimletInstance.sensitivity[composeViewId];
+      switch (sensitivity) {
+         case 'Personal':
+            customHeaders.push({name: "Sensitivity", _content: 'Personal'});
+            break;
+         case 'Private':
+            customHeaders.push({name: "Sensitivity", _content: 'Private'});
+            break;
+         case 'Company-Confidential':
+            customHeaders.push({name: "Sensitivity", _content: 'Company-Confidential'});
+            break;
+         default:
+            customHeaders.push({name: "Sensitivity", _content: 'Normal'});
+      }
+      zimletInstance.resetSensitivity();
+   }
+};
+
